20f1b70c4199bf561a7cd404971507be8b38e34b
MateoCardonaRincon <mcardonari@unal.edu.co>
2021-11-08 22:16:09 -0500

    Presentación Mateo Cardona Rincón

--- client/src/components/Presentacion.jsx
+++ client/src/components/Presentacion.jsx
@@ -6,6 +6,11 @@ const Presentacion = () => {
         <span>CC: </span> 1037586396 <br />
         <span>Email: </span> diego.valencr@gmail.com
       </p>
+      <p>
+        <span>Nombre:</span> Mateo Cardona Rincón <br />
+        <span>CC: </span> 1094951174 <br />
+        <span>Email: </span> mcardonari@unal.edu.co
+      </p>
     </div>
   );
 };
