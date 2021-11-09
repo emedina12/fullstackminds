50104a5d53ee4d1ee18adc9af0b2cae46bb6f95f
Einer <einermedina15@gmail.com>
2021-11-09 09:46:55 -0500

    Commit de presentación Einer Medina

--- client/src/components/Presentacion.jsx
+++ client/src/components/Presentacion.jsx
@@ -6,6 +6,11 @@ const Presentacion = () => {
         <span>CC: </span> 1037586396 <br />
         <span>Email: </span> diego.valencr@gmail.com
       </p>
+      <p>
+        <span>Nombre:</span>Einer de Jesús Medina Fernandéz <br />
+        <span>CC: </span> 1045748822 <br />
+        <span>Email: </span> einermedina15@gmail.com
+      </p>
     </div>
   );
 };
